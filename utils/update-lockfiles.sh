f346857b2457a2836071981cd280931329709356
Claudio Usai <c.usai186@gmail.com>
2025-12-30 11:00:20 +0100

    utils: generate lockfiles for versions 3.14 and 3.14t

--- utils/update-lockfiles.sh
+++ utils/update-lockfiles.sh
@@ -13,12 +13,23 @@ else
     echo "Found $PDM_VERSION"
 fi
 
-SUPPORTED_PYTHON_VERSIONS=("3.10" "3.11" "3.12" "3.13")
+SUPPORTED_PYTHON_VERSIONS=("3.10" "3.11" "3.12" "3.13" "3.14" "3.14t")
 
 ERROR=0
 
 for spv in "${SUPPORTED_PYTHON_VERSIONS[@]}"
 do
+    # check if python version string ends with 't' (for 'threaded')
+    # if yes just create a symlink to the non-threaded lockfile
+    if [[ $spv == *"t" ]]; then
+        base_spv=${spv%"t"}
+        echo "Creating symlink for py$spv.lock to py$base_spv.lock"
+        ln -sf "py$base_spv.lock" "py$spv.lock"
+        continue
+    fi
+
+    echo "Updating lockfile for py$spv.lock"
+
     pdm lock --python="==$spv.*" --lockfile=py$spv.lock
 
     if [[ $? -ne 0 ]]; then
